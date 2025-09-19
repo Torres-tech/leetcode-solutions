b982770327c080da44c0f4a30e562e31f1c3c0d9
Torres-tech <181374885+Torres-tech@users.noreply.github.com>
2025-09-19 12:21:39 -0500

    Update twoPointers.java

--- java/twoPointers/twoPointers.java
+++ java/twoPointers/twoPointers.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
+        //create the left and right pointers
         int i = 0 , j = s.length() -1;
         while (i<j) {
-
              // skip non-alphanumeric on the left
             while(i<j && !Character.isLetterOrDigit(s.charAt(i))) i++;
             // skip non-alphanumeric on the right
             while(j>i && !Character.isLetterOrDigit(s.charAt(j))) j--;
-
-
-            
+            //if left character is not the same at the right character, then return false
             if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
             return false;
+            //if they are the same, keep moving
             i++;
             j--;
         } return true;
